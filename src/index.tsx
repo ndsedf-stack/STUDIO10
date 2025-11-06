94de5089ee97a8bfd7487fb9427f83ff1bfc8a60
ndsedf-stack <ndsedf@gmail.com>
2025-11-06 18:18:17 +0100

    Convert index.js to TypeScript as index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { CalendarHeatmap } from './components/CalendarHeatmap';
+import { MuscleGroupHeatmap } from './components/MuscleGroupHeatmap';
+import { ProgressionChart } from './components/ProgressionChart';
+import { RestTimer } from './components/RestTimer';
+import { IntensificationStep } from './components/IntensificationStep';
+import { TechniqueHighlight } from './components/TechniqueHighlight';
+import { SetsTracker } from './components/SetsTracker';
+import { ActiveWorkoutView } from './components/ActiveWorkoutView';
+import { StatisticsView } from './components/StatisticsView';
+import { WorkoutPlannerView } from './components/WorkoutPlannerView';
+import { BottomNav } from './components/BottomNav';
+import { App } from './components/App';
 
-const container = document.getElementById('root');
-if (!container) {
-  throw new Error('Root element not found');
-}
+const workoutData: any[] = []; // Replace 'any' with appropriate type
 
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App 
+      workoutData={workoutData}
+      // Other props can be defined as per original functionality
+    />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
