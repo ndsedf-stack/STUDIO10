8a009b636332a7268852bb957b300b0f350180aa
ndsedf-stack <ndsedf@gmail.com>
2025-11-06 18:33:15 +0100

    Add icons: Dumbbell, Chart, and Plus

---
+++ src/components/Icons.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const DumbbellIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M21 8.5C21 7.12 19.88 6 18.5 6H17V4.5C17 3.12 15.88 2 14.5 2h-5C8.12 2 7 3.12 7 4.5V6H5.5C4.12 6 3 7.12 3 8.5v7C3 16.88 4.12 18 5.5 18H7v1.5C7 20.88 8.12 22 9.5 22h5c1.38 0 2.5-1.12 2.5-2.5V18h1.5c1.38 0 2.5-1.12 2.5-2.5v-7zM9 4.5c0-.28.22-.5.5-.5h5c.28 0 .5.22.5.5V6H9V4.5zm10 11c0 .28-.22.5-.5.5H17v-8h1.5c.28 0 .5.22.5.5v7zM7 8v8H5.5c-.28 0-.5-.22-.5-.5v-7c0-.28.22-.5.5-.5H7zm8 11.5c0 .28-.22.5-.5.5h-5c-.28 0-.5-.22-.5-.5V18h6v1.5z"/>
+  </svg>
+);
+
+export const ChartIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M16 6H18V20H16V6ZM11 11H13V20H11V11ZM6 16H8V20H6V16Z"/>
+  </svg>
+);
+
+export const PlusIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+    <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z"/>
+  </svg>
+);
